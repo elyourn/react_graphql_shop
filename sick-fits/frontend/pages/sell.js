f1bbed75daa8a0ce7af3cd39d125525090e3bd66
elyourn <elyourn52@gmail.com>
2019-02-09 15:01:21 +0300

    Frontend: Routing + Styled Components

---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Link from 'next/link';
+
+class Sell extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Sell!</p>
+            </div>
+        )
+    }
+}
+
+export default Sell;
